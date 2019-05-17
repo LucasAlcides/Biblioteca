75f637248ec9cbfb4a771224db34036713e1997d
LucasAlcides <lucasaalcides@gmail.com>
2019-05-16 22:04:12 -0400

    Criado o Projeto Laboratório e adcionado o Entity

---
+++ AlcidesLaboratorio/AlcidesLaboratorio/Contexto/LaboratorioContexto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlcidesLaboratorio.Contexto
+{
+    public class LaboratorioContexto
+    {
+    }
+}
---
+++ AlcidesLaboratorio/AlcidesLaboratorio/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AlcidesLaboratorio.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
